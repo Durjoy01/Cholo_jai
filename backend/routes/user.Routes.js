2ee97d36f5cdeac5946d7741298aa87c0d17725c
tFardaus <fardauspriom.czs@gmail.com>
2024-11-28 14:18:03 +0600

    Update user.Routes.js

--- backend/routes/user.Routes.js
+++ backend/routes/user.Routes.js
@@ -1,1 +1,12 @@
+import express from 'express';
+import { register, login, googleLogin, updateUser } from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/google-login', googleLogin);
+router.put('/update/:id', updateUser);
+
+export default router;
 
